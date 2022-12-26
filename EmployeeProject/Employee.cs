0419f0b904d6b7d42d1affb17dd2f4e08b36c120
snehaps <swamisneha124@gmail.com>
2022-12-26 18:35:19 +0530

    EmployeeDetail

---
+++ EmployeeProject/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maveric.Runner
+{
+    public class Employee
+    {
+        public int empId;
+        public string empName;
+        public double empSalary;
+        public static string companyName;
+        public static string companyLocation;
+        public char empPerformance;
+
+        public void PrintEmployeeDetail()
+        {
+
+            Console.WriteLine("Employee ID: " + empId);
+            Console.WriteLine("Employee Name:" + empName);
+            Console.WriteLine("Employee Salary: " + empSalary);
+            Console.WriteLine("Employee Performance: " + empPerformance);
+            Console.WriteLine("Company Name: " + Employee.companyName);
+            Console.WriteLine("Company Location: " + Employee.companyLocation);
+            Console.WriteLine("-------------------------------------------------");
+        }
+        //CalculateBonus
+
+        public void GetGrossSalarywithBonus()
+        {
+            Console.WriteLine("Employee Id: " + empId);
+            if (empPerformance == 'A')
+            {
+                Console.WriteLine("25%");
+                Console.WriteLine(empSalary + (empSalary * 25 / 100));
+            }
+            else if (empPerformance == 'B')
+            {
+                Console.WriteLine("15%");
+                Console.WriteLine(empSalary + (empSalary * 15 / 100));
+            }
+            else if (empPerformance == 'C')
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+
---
+++ EmployeeProject/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Maveric.Runner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Dashboard");
+            Employee.companyName = "Maveric";
+            Employee.companyLocation = ("Chennai");
+
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+            Employee emp3 = new Employee();
+            Employee emp4 = new Employee();
+
+            emp1.empId = 101;
+            emp1.empName = "saul";
+            emp1.empSalary = 9000;
+            emp1.empPerformance = 'B';
+
+            Console.WriteLine(emp1.empId);
+            Console.WriteLine(emp1.empName);
+            Console.WriteLine(emp1.empSalary);
+
+            emp2.empId = 102;
+            emp2.empName = "Kim";
+            emp2.empSalary = 12000.2;
+            emp1.empPerformance = 'A';
+            Console.WriteLine(emp2.empId);
+            Console.WriteLine(emp2.empName);
+            Console.WriteLine(emp2.empSalary);
+           
+
+            emp3.empId = 103;
+            emp3.empName = "Jack";
+            emp3.empSalary = 6000;
+            emp1.empPerformance = 'C';
+            Console.WriteLine(emp3.empId);
+            Console.WriteLine(emp3.empName);
+            Console.WriteLine(emp3.empSalary);
+
+            emp1.PrintEmployeeDetail();
+            emp2.PrintEmployeeDetail();
+            emp3.PrintEmployeeDetail();
+
+            emp1.GetGrossSalarywithBonus();
+            emp2.GetGrossSalarywithBonus();
+            emp3.GetGrossSalarywithBonus();
+        }
+    }
+}
